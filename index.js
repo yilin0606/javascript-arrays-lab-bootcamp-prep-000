29c97a087f3a1e09124d8bf042b2a9b124204dec
Yilin Freya Gao <thisisgyl@gmail.com>
2019-10-11 23:39:53 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var kittens = ['Milo','Otis','Garfield']
 // Add your functions and code here
 
 function destructivelyAppendKitten(name) {
-  var name_1=name.push("Ralph");
-  return name_1;
+   name.push("Ralph");
+   return name
 }
 
